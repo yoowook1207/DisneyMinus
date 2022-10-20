f8f88fe924a12df099a370faeac5df93093c8dfe
Sung Yoo <yoowook1207@gmail.com>
2022-10-20 08:53:48 -0400

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    RegisterPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
