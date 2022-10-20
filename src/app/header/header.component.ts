fce7ad4cc60484c718b53b8459798b5f1cae876f
Sung Yoo <yoowook1207@gmail.com>
2022-10-20 11:03:27 -0400

    header login/out function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isLoggedIn :boolean = true;
+  userAccountDisplay : string = " none";
+  loginDisplay : string = "block";
+  userName : string = "Sung"
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    if(this.isLoggedIn) {
+      this.userAccountDisplay = "block"
+      this.loginDisplay = "none"
+    } else {
+      this.userAccountDisplay = "none"
+      this.loginDisplay = "block"
+    }
+    
+  }
 }
